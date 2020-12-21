4714ffcf5b63656b533acdb28eb621d5cd99f255
harshit saxena <harshitsaxena586@gmail.com>
2020-12-22 02:26:13 +0530

    feat:added the Api

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var button=document.getElementById("clickHere")
 var inputText=document.querySelector("textArea")
+var outputText=document.querySelector("output")
+
+function constructURL(text){
+   return "https://api.funtranslations.com/translate/minion.json" + "?" + "text="+ text
+}
+
 
 function clickHandler(){
-   console.log("welcome "+ inputText.value)
+   // outputText.innerHTML("testing you are ")
+   var userInput= inputText.value
+   
+      fetch(constructURL(userInput))
+      .then(response=> response.json())
+      .then(json=> console.log(json.contents.translated))
 
+   
 
 }
 button.addEventListener("click",clickHandler)
 
-url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-fetch
+
